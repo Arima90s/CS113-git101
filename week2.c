818e25fc5f35c6ee94512f51d69131f2356a35fe
Itsaraphap <Itsaraphap.s@ku.th>
2020-02-12 23:02:20 +0700

    Types, Operators and Expressions

---
+++ week2.c
@@ -0,0 +1,47 @@
+#include <stdio.h> //stdio -> standard input output
+#include <stdlib.h> //stdlib -> standard library
+// char name[20];
+char number[20]; //character 
+// int number;
+
+int main() {
+   // printf("Enter name : ");
+   // gets(name);
+   // fgets(name, 5, stdin); --> 5 is buffer size that we define
+   // getchar(chr); --> can only get 1 character according to ascii table. when enter, they won't send anything back
+   // fgets(number, 5, stdin);
+   // int i = atoi(number); --> array to integer
+   // printf("number = %s, i = %d",number,i);
+   // putchar(100); --> work like "printf", they will send output "d" to terminal
+   // putchar('u'); 
+
+   // --------------------------------------------------------------------------------------
+
+   int found = 0;
+   float isExist = 0.0;
+   char isOnline = '\0'; // \0 คือ NULL
+   // all of the above are "Fale boolean"
+
+   // fgets(number, 5, stdin);
+   // int i = atoi(number);
+   // if (i  > 0 && i <= 10)
+   // {
+   //    printf("if = %d\n", i > 0 && i<=10); --> if "i > 0 && i<=10" is True then output will be "1"
+   // }
+
+   // --------------------------------------------------------------------------------------
+
+   fgets(number, 5, stdin);
+   int i = atoi(number);
+   // int count = 0;
+   // while (count < i){
+   //    printf("%d\n",count);
+   //    count++;
+   // }
+
+   // --------------------------------------------------------------------------------------
+
+   for (int j=0; j<i; j++){
+      printf("j = %d\n", j);
+   }
+}
